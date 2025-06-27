dc0a4c28b21ef2971941e08c8527a640377d7eb3
Souliya Phoupaseuth <souliyapps@gmail.com>
2025-06-27 12:34:31 +0700

    🐞 fix(deploy):

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,33 @@
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import preserveDirectives from 'rollup-preserve-directives';
 import { defineConfig } from 'vite';
+import path from 'path';
+import fs from 'fs';
+import react from '@vitejs/plugin-react';
+// import { visualizer } from 'rollup-plugin-visualizer';
+import preserveDirectives from 'rollup-preserve-directives';
 
+// https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
   let aliases: Record<string, string> = {};
   if (fs.existsSync(path.resolve(__dirname, '../../packages'))) {
-    const packages = fs.readdirSync(path.resolve(__dirname, '../../packages'));
+    const packages = fs.readdirSync(
+      path.resolve(__dirname, '../../packages')
+    );
     aliases = {
-      'data-generator-retail': path.resolve(__dirname, '../data-generator/src'),
+      'data-generator-retail': path.resolve(
+        __dirname,
+        '../data-generator/src'
+      ),
     };
     for (const dirName of packages) {
       if (dirName === 'create-react-admin') continue;
       const packageJson = JSON.parse(
         fs.readFileSync(
-          path.resolve(__dirname, '../../packages', dirName, 'package.json'),
+          path.resolve(
+            __dirname,
+            '../../packages',
+            dirName,
+            'package.json'
+          ),
           'utf8'
         )
       );
@@ -29,10 +41,10 @@ export default defineConfig(async ({ mode }) => {
   return {
     plugins: [
       react(),
-      //   visualizer({
-      //     open: process.env.NODE_ENV !== 'CI',
-      //     filename: './dist/stats.html',
-      //   }),
+      // visualizer({
+      //   open: process.env.NODE_ENV !== 'CI',
+      //   filename: './dist/stats.html',
+      // }),
     ],
     define: {
       'process.env': process.env,
@@ -41,25 +53,14 @@ export default defineConfig(async ({ mode }) => {
       port: 8000,
       open: true,
     },
-    base: '/',
+    base: './',
     esbuild: {
       keepNames: true,
     },
     build: {
       sourcemap: true,
-      chunkSizeWarningLimit: 1500, // increase default limit (default is 500)
       rollupOptions: {
         plugins: [preserveDirectives()],
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              if (id.includes('react')) return 'react';
-              if (id.includes('@mui')) return 'mui';
-              if (id.includes('lodash')) return 'lodash';
-              return 'vendor';
-            }
-          },
-        },
       },
     },
     resolve: {
@@ -77,18 +78,14 @@ export default defineConfig(async ({ mode }) => {
           find: 'react-router-dom',
           replacement: path.resolve(
             __dirname,
-            `node_modules/react-router/dist/${
-              mode === 'production' ? 'production' : 'development'
-            }/index.mjs`
+            `node_modules/react-router/dist/${mode === 'production' ? 'production' : 'development'}/index.mjs`
           ),
         },
         {
           find: 'react-router',
           replacement: path.resolve(
             __dirname,
-            `node_modules/react-router/dist/${
-              mode === 'production' ? 'production' : 'development'
-            }/index.mjs`
+            `node_modules/react-router/dist/${mode === 'production' ? 'production' : 'development'}/index.mjs`
           ),
         },
         // The 2 next aliases are needed to avoid having multiple MUI instances
@@ -100,7 +97,7 @@ export default defineConfig(async ({ mode }) => {
           )}`,
         },
         // we need to manually follow the symlinks for local packages to allow deep HMR
-        ...Object.keys(aliases).map((packageName) => ({
+        ...Object.keys(aliases).map(packageName => ({
           find: packageName,
           replacement: aliases[packageName],
         })),
