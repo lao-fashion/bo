2570ba4e3ae4e57bc721887c7e829ac286b0fda2
SouliyaPPS-7575 <SouliyaPPS-7575@users.noreply.github.com>
2025-06-27 03:48:13 +0000

    deploy: c9b4f8a36fbf7824829feb52ad12f99c14e116f6

---
+++ assets/rest-C4GCvv4K.js
@@ -0,0 +1,2 @@
+import{by as t}from"./vendor-D9aWMbm2.js";import"./lodash-Da20LJ-N.js";import"./react-DWqYkt8H.js";import"./mui-BxrV_jwC.js";const e=t("http://localhost:4000");export{e as default};
+//# sourceMappingURL=rest-C4GCvv4K.js.map
