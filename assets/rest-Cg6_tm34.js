c0d81d48dd39d9a59da2edab977c222ecbfc6f33
SouliyaPPS-7575 <SouliyaPPS-7575@users.noreply.github.com>
2025-07-13 06:31:48 +0000

    deploy: 721a41cb4ea495c5e8d68927aef8ffc1d2d4b70d

--- assets/rest-Cg6_tm34.js
+++
@@ -1,2 +0,0 @@
-import{by as t}from"./vendor-B8RineCB.js";import"./lodash-Bo37TYK2.js";import"./react-BnqDHuo8.js";import"./mui-vtje_hVu.js";const e=t("http://localhost:4000");export{e as default};
-//# sourceMappingURL=rest-Cg6_tm34.js.map
---
+++ assets/rest-x0bN8bj8.js
@@ -0,0 +1,2 @@
+import{s as t}from"./index-dpCNeGPL.js";const o=t("http://localhost:4000");export{o as default};
+//# sourceMappingURL=rest-x0bN8bj8.js.map
