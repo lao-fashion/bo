7f9da88332ed46d3d1abbcbe55173fe9e72a3fa8
anousonefs <anousonefs@users.noreply.github.com>
2025-07-13 13:09:46 +0000

    deploy: 38934bd82514c129bd7f8d1455513c180c089376

---
+++ assets/rest-9_6A559S.js
@@ -0,0 +1,2 @@
+import{s as t}from"./index-qoyEp6pe.js";const o=t("http://localhost:4000");export{o as default};
+//# sourceMappingURL=rest-9_6A559S.js.map
--- assets/rest-BTuO5KvR.js
+++
@@ -1,2 +0,0 @@
-import{s as t}from"./index-Do8U8IBe.js";const o=t("http://localhost:4000");export{o as default};
-//# sourceMappingURL=rest-BTuO5KvR.js.map
