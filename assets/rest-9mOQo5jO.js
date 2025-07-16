2dc5ae94a05c9c9242ba7416495e998ec1cfd9d9
SouliyaPPS-7575 <SouliyaPPS-7575@users.noreply.github.com>
2025-07-16 15:00:28 +0000

    deploy: 9ee219526eadc293899859952390c182e6619969

---
+++ assets/rest-9mOQo5jO.js
@@ -0,0 +1,2 @@
+import{s as e}from"./index-BOgC7UeN.js";var s={};const o=e(s.REACT_APP_API_BASE_URL||"http://localhost:4000");export{o as default};
+//# sourceMappingURL=rest-9mOQo5jO.js.map
--- assets/rest-dHXceWQ0.js
+++
@@ -1,2 +0,0 @@
-import{s as e}from"./index-R72-6OBv.js";var s={};const o=e(s.REACT_APP_API_BASE_URL||"http://localhost:4000");export{o as default};
-//# sourceMappingURL=rest-dHXceWQ0.js.map
